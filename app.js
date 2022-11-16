cdd41f11ac7e4c9919df77a601a99baa3b4a9514
Ivan Grekov <ivan.grekov@windmill.digital>
2022-11-16 19:53:51 +0200

    additional checks with command-line

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+//#region native process.env (X=1 node app.js)
+// console.log('process.env', process.env);
+// console.log('process.env.X', process.env.X);
+//#endregion
+
 //#region  process.env[variable]
 // import dotenv from 'dotenv';
 
@@ -9,16 +14,23 @@
 //#endregion
 
 //#region path.resolve
-// import path from 'path';
-
-// console.log(path.resolve('./src/app.js'));
+// console.log('process.env.PATH', process.env.PATH);
+// console.log('-----');
+// console.log(require.resolve.paths('a'));
 // console.log(path.resolve('./src'));
+//#endregion
+
+//#region native argv (node app.js x=1 y=2)
+// console.log('process.argv', process.argv.slice(2));
 //#endregion
 
 //#region minimist
 // import minimist from 'minimist';
 
 // const argv = minimist(process.argv.slice(2));
+// console.log('argv', argv);
+
+// NOTE: for long var names use -- instead of - (optimal --name=Ivan)
 // const userName = argv.name;
 
 // if (userName) {
@@ -27,17 +39,15 @@
 //#endregion
 
 //#region readline (stdin / stdout)
-// import readline from 'readline';
+import readline from 'readline';
 
-// const terminalInterface = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// });
+const terminalInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-// terminalInterface.question("What's your name? ", (answer) => {
-//     const userName = answer || 'stranger';
-
-//     console.log(`Hello, ${userName}`);
-//     terminalInterface.close();
-// });
+terminalInterface.question('What is your name? ', (answer) => {
+    console.log('Hello,', answer || 'stranger');
+    terminalInterface.close();
+});
 //#endregion
