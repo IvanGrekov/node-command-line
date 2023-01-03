f639453d1b86a616bab4daadec50f786f0f286bd
Ivan Grekov <ivan.grekov@windmill.digital>
2023-01-03 12:47:22 +0200

    read env vars

---
+++ read-env-vars.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// console.log(process.env);
+// console.log(process.env.X);
+// console.log(process.env.PATH);
+// console.log(+process.env.Y + +process.env.X);
