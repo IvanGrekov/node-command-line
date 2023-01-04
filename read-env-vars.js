fe96467c29f0f02a80c0cac33a7fa42c444c9d41
Ivan Grekov <ivan.grekov@windmill.digital>
2023-01-04 12:59:44 +0200

    script to use dotenv

--- read-env-vars.js
+++ read-env-vars.js
@@ -1,5 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// NOTE: to don't import, use `node -r dotenv/config read-env-vars.js`
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 // console.log(process.env);
 // console.log(process.env.X);
